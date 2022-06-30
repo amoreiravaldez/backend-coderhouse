7dc4f68da337d59a3f7fa9aa91cdebb9144ff6ce
Alvaro Moreira <amoreiravaldez@gmail.com>
2022-06-29 23:13:45 -0300

    Desafio 5

--- desafio-5/servidor.js
+++ desafio-5/servidor.js
@@ -1,19 +1,6 @@
-/*
-
-const http = require('http');
-
-const server = http.createServer((req,res) => {
-    res.end();
-})
-
-const PORT = 8080;
-
-const connectedServer = server.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`);
-})
-
-127.0.0.1 */
-
+const fs = require("fs");
+let productArray = [];
+let productObject = {};
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -23,15 +10,47 @@ const server = app.listen(PORT, () => {
 });
 
 server.on("error", error => console.log(`Error en servidor ${error}`));
- 
+
+// Clase contenedor
+
+class Contenedor {  
+    constructor(nombreDelArchivo) {
+        this.nombreDelArchivo = "./" + nombreDelArchivo;
+    }
+
+    async getById(id) {
+        try {
+            productArray = JSON.parse(await fs.promises.readFile(this.nombreDelArchivo, "utf8"));
+            let productById = productArray.find((product) => product.id == id);
+            if (productById === undefined ) {return null } else { return productById};
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getAll() {
+        try {
+            productArray = JSON.parse(await fs.promises.readFile(this.nombreDelArchivo, "utf8"));
+            console.log(productArray)
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
 app.get('/', (req,res) => {
-    res.send({mensaje: 'hola mundo'})
+    res.send(`<h1>Bienvenido a mi nuevo servidor express en el puerto ${PORT}</h1>`);
 });
 
 app.get('/productos', (req,res) => {
-    res.send({mensaje: 'hola mundo'})
+    const Productos = new Contenedor("productos.txt");
+    res.send(Productos.getAll());
 });
 
 app.get('/productoRandom', (req,res) => {
-    res.send({mensaje: 'hola mundo'})
+    //res.send({mensaje: 'hola mundo'})
 });
+
+
